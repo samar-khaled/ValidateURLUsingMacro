8cb83bb8e728110808e7ee3d34c226999364b201
Samar Khaled <samar.khaled@gmail.com>
2025-04-08 13:15:27 +0200

    Initial Commit

---
+++ URLMacro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  URLMacro
+//
+//  Created by Samar Khaled on 08/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ URLMacro/URLMacroApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLMacroApp.swift
+//  URLMacro
+//
+//  Created by Samar Khaled on 08/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct URLMacroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
